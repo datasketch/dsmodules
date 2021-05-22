64080c9f712a8aefec08ffc1a5e8fb2de0d9d7a3
Lena Mangold <lena@mangold.org>
2021-05-22 10:26:41 -0500

    include comment with alternative using htmldependency

--- tests/testthat/test-downloadImage.R
+++ tests/testthat/test-downloadImage.R
@@ -6,6 +6,15 @@ test_that("google font stylesheet added", {
                                            col2 = c(1, 2)),
                                 style = "font-family: Lato;")
 
+  # table$dependencies <- list(
+  #   htmltools::htmlDependency(
+  #     name = "googlefonts",
+  #     version = as.character("2.0"),
+  #     src = c(href = "https://fonts.googleapis.com/css?family=Lato"),
+  #     stylesheet = ""
+  #   )
+  # )
+
   opts <- dsvizopts::merge_dsviz_options(title_family = "Lato")
 
   table_with_font <- import_google_font(viz = table, opts_theme = opts$theme)
@@ -13,6 +22,8 @@ test_that("google font stylesheet added", {
   expect_equal(table_with_font$prepend[[2]]$name, "link")
   expect_equal(table_with_font$prepend[[2]]$attribs, list(href = "https://fonts.googleapis.com/css?family=Lato",
                                                           rel = "stylesheet"))
+
+  htmlwidgets::saveWidget(table, file = "tests/testthat/table_with_font.html")
 
 })
 
