6507a70787518a1d41af8d9c0179546a32037c15
Lena Mangold <lena@mangold.org>
2021-05-31 08:24:59 -0500

    specify logo in opts theme

--- tests/testthat/test-extrahtml.R
+++ tests/testthat/test-extrahtml.R
@@ -10,8 +10,8 @@ test_that("google font stylesheet added", {
 
   table_with_font <- import_google_font(viz = table, opts_theme = opts$theme)
 
-  expect_equal(table_with_font$prepend[[2]]$name, "style")
-  expect_equal(table_with_font$prepend[[2]]$children[[1]], "@import url('https://fonts.googleapis.com/css?family=Lato');")
+  expect_equal(table_with_font$prepend[[1]]$name, "style")
+  expect_equal(table_with_font$prepend[[1]]$children[[1]], "@import url('https://fonts.googleapis.com/css?family=Lato');")
 
 
   library(lfltmagic)
@@ -26,13 +26,10 @@ test_that("logo added to reactable", {
   table <- reactable::reactable(data.frame(col1 = c('a', 'b'),
                                            col2 = c(1, 2)))
 
-  opts_theme <- dsvizopts::merge_dsviz_options(branding_include = TRUE)$theme
+  opts_theme <- dsthemer::dsthemer_get(org = "public")
+  opts_theme$logo <- "public"
 
-  opts_theme1 <- dsthemer::dsthemer_get(org = "public")
-
-
-  table_with_logo <- add_logo_reactable(table = table, opts_theme = opts_theme1)
-  table_with_logo
+  table_with_logo <- add_logo_reactable(table = table, opts_theme = opts_theme)
 
   expect_equal(table_with_logo$append[[1]]$name, "img")
   expect_match(table_with_logo$append[[1]]$attribs$src, "^data:image/")
