d7894e3f2e1319e6e8541f2341dac8e134cdd963
Lena Mangold <lena@mangold.org>
2021-05-31 02:25:37 -0500

    rename test extra html file

--- tests/testthat/test-extrahtml.R
+++ tests/testthat/test-extrahtml.R
@@ -6,12 +6,20 @@ test_that("google font stylesheet added", {
                                            col2 = c(1, 2)),
                                 style = "font-family: Lato;")
 
-  opts <- dsvizopts::merge_dsviz_options(title_family = "Lato")
+  opts <- dsvizopts::merge_dsviz_options(title = "some title for this", text_family = "Lato")
 
   table_with_font <- import_google_font(viz = table, opts_theme = opts$theme)
 
   expect_equal(table_with_font$prepend[[2]]$name, "style")
   expect_equal(table_with_font$prepend[[2]]$children[[1]], "@import url('https://fonts.googleapis.com/css?family=Lato');")
+
+
+  library(lfltmagic)
+  map <- lflt_choropleth_Gcd(sample_data("Gcd"), opts = opts)
+
+  map_with_font <- import_google_font(viz = map, opts_theme = opts$theme)
+
+  htmlwidgets::saveWidget(map_with_font, "tests/testthat/map_with_font.html")
 
 })
 
@@ -20,10 +28,13 @@ test_that("logo added to reactable", {
   table <- reactable::reactable(data.frame(col1 = c('a', 'b'),
                                            col2 = c(1, 2)))
 
-  opts <- dsvizopts::merge_dsviz_options(branding_include = TRUE)
+  opts_theme <- dsvizopts::merge_dsviz_options(branding_include = TRUE)$theme
+
+  opts_theme1 <- dsthemer::dsthemer_get(org = "public")
 
 
-  table_with_logo <- add_logo_reactable(table = table, opts_theme = opts$theme)
+  table_with_logo <- add_logo_reactable(table = table, opts_theme = opts_theme1)
+  table_with_logo
 
   expect_equal(table_with_logo$append[[1]]$name, "img")
   expect_match(table_with_logo$append[[1]]$attribs$src, "^data:image/")
